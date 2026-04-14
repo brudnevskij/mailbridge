c3852368199228ac8ee6ff742469e085f7f171d1
Segfaulter <brudnevskij@users.noreply.github.com>
2026-04-14 23:35:18 +0300

    tests: added tests for /subscribe handler
    - test for code 200
    - test for 400

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -15,7 +15,6 @@ async fn spawn_app() -> String {
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app().await;
-
     let client = reqwest::Client::new();
 
     let response = client
@@ -26,4 +25,49 @@ async fn health_check_works() {
 
     assert!(response.status().is_success(), "health check works");
     assert_eq!(response.content_length(), Some(0));
+}
+
+#[tokio::test]
+async fn subscribe_return_a_200_for_valid_form() {
+    let address = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(format!("{}/subscribe", address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn subscribe_returna_400_on_invalid_data() {
+    let address = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (body, error_message) in test_cases {
+        let response = client
+            .post(format!("{}/subscribe", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "API did not fail with 400 code when payload was: {}",
+            error_message
+        );
+    }
 }
