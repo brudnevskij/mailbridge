bf610e4629c3ab7dd7027b3bf40711096ffcd556
Segfaulter <brudnevskij@users.noreply.github.com>
2026-04-14 23:04:02 +0300

    refactor: adjusted tests to work with listener setup

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,19 +1,25 @@
+use std::net::TcpListener;
+
 use mailbridge::run;
 
-async fn spawn_app() {
-    let srv = run().await.expect("should start");
+async fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let srv = run(listener).await.expect("should start");
 
     let _ = tokio::spawn(srv);
+
+    format!("127.0.0.1:{}", port)
 }
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app().await;
+    let address = spawn_app().await;
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("http://{}/health_check", &address))
         .send()
         .await
         .expect("Failed to send request");
