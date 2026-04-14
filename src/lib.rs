ad8b6bef7710c0e00cea5e3d7be488a74592f52a
Segfaulter <brudnevskij@users.noreply.github.com>
2026-04-14 23:41:59 +0300

    feat: added subscribe handler, parsing the form

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,33 @@
 use std::net::TcpListener;
 
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, dev::Server, web};
+use actix_web::{
+    App, HttpRequest, HttpResponse, HttpServer, Responder,
+    dev::Server,
+    web::{self, Form},
+};
 
-async fn health_check(_req: HttpRequest) -> impl Responder {
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(form: Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
 pub async fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("health_check", web::get().to(health_check))
+            .route("subscribe", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
