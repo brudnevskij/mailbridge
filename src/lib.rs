0e955b3f03ff0e8a915565597a78bb908f9cc9f0
Segfaulter <brudnevskij@users.noreply.github.com>
2026-04-14 23:01:34 +0300

    refactor: refactored run to accept listener

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
+use std::net::TcpListener;
+
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, dev::Server, web};
 
 async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> Result<Server, std::io::Error> {
+pub async fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
+        .listen(listener)?
         .run();
 
     Ok(server)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::net::TcpListener;
+
 use mailbridge::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let server = run().await?;
+    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+    let server = run(listener).await?;
     server.await
 }
