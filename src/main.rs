294049400d34084e9b77f3745c74a575f0ce1c2e
Segfaulter <brudnevskij@users.noreply.github.com>
2026-04-23 20:03:28 +0300

    feat: added configuration with deb envs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use mailbridge::{configuration::get_configuration, run};
 use std::net::TcpListener;
-
-use mailbridge::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     let server = run(listener).await?;
     server.await
 }
