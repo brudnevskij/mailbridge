eb85d57347cb6225b20e557aab04e9a189175ba3
Segfaulter <brudnevskij@users.noreply.github.com>
2026-05-04 01:48:34 +0300

    feat: added validation for sub name

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -5,7 +5,8 @@ use actix_web::{
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
-use unicode_segmentation::UnicodeSegmentation;
+
+use crate::domain::{NewSubscriber, SubscriberName};
 
 #[derive(serde::Deserialize, Debug)]
 pub struct FormData {
@@ -21,10 +22,9 @@ pub struct FormData {
         subscriber_name = %form.name)
 )]
 pub async fn subscribe(form: Form<FormData>, pool: Data<PgPool>) -> HttpResponse {
-    if !is_valid_name(&form.name){
-        return HttpResponse::BadRequest().finish();
-    }
-    match insert_subscriber(&pool, &form).await
+    let new_subscriber = NewSubscriber{ email: form.0.email, name: SubscriberName::parse(form.0.name) };
+
+    match insert_subscriber(&pool, &new_subscriber).await
     {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) =>  HttpResponse::InternalServerError().finish(), 
@@ -33,17 +33,17 @@ pub async fn subscribe(form: Form<FormData>, pool: Data<PgPool>) -> HttpResponse
 
 #[tracing::instrument(
     name = "Saving new subscriber in the database",
-    skip(form, pool)
+    skip(new_subscriber, pool)
 )]
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData)-> Result<(), sqlx::Error>{
+pub async fn insert_subscriber(pool: &PgPool, new_subscriber: &NewSubscriber)-> Result<(), sqlx::Error>{
 sqlx::query!(
         r#"
             INSERT INTO subscriptions (id, email, name, subscribed_at)
             VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email,
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
@@ -55,11 +55,3 @@ sqlx::query!(
     Ok(())
 }
 
-pub fn is_valid_name(name :&str)->bool{
-    let is_empty_or_whitespace = name.trim().is_empty();
-    let is_too_long = name.graphemes(true).count()> 256;
-    let forbidden_characters = ['/', '(',')', '"','<','>'];
-    let contains_forbidden_characters = name.contains(forbidden_characters);
-
-    !(is_empty_or_whitespace|| is_too_long || contains_forbidden_characters)
-}
