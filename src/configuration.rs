762b289f48fdc7f490791802672326bbfec82194
Segfaulter <brudnevskij@users.noreply.github.com>
2026-05-03 23:14:56 +0300

    feat: added env deserialization

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,4 +1,5 @@
 use secrecy::{ExposeSecret, Secret};
+use serde_aux::field_attributes::deserialize_number_from_string;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -10,6 +11,7 @@ pub struct Settings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
@@ -17,6 +19,7 @@ pub struct DatabaseSettings {
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSetting {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -25,18 +28,16 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let mut settings = config::Config::default();
     let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("configuration");
-
     settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
-
     let environment: Environment = std::env::var("APP_ENVIRONMENT")
         .unwrap_or_else(|_| "local".into())
         .try_into()
-        .expect("Failed to parse APP_ENVIRONMENT");
-
+        .expect("Failed to parse APP_ENVIRONMENT.");
     settings.merge(
         config::File::from(configuration_directory.join(environment.as_str())).required(true),
     )?;
 
+    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
     settings.try_into()
 }
 
