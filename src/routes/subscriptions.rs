087c916053425f634e45d98eeabbeb92620db18d
Segfaulter <brudnevskij@users.noreply.github.com>
2026-05-04 01:20:11 +0300

    feat: added naive validation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,12 +1,11 @@
 use core::result::Result::{self, Ok};
-
 use actix_web::{
-    HttpResponse,
-    web::{Data, Form},
+    HttpResponse, web::{Data, Form}
 };
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+use unicode_segmentation::UnicodeSegmentation;
 
 #[derive(serde::Deserialize, Debug)]
 pub struct FormData {
@@ -22,6 +21,9 @@ pub struct FormData {
         subscriber_name = %form.name)
 )]
 pub async fn subscribe(form: Form<FormData>, pool: Data<PgPool>) -> HttpResponse {
+    if !is_valid_name(&form.name){
+        return HttpResponse::BadRequest().finish();
+    }
     match insert_subscriber(&pool, &form).await
     {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -51,4 +53,13 @@ sqlx::query!(
             e
         })?;
     Ok(())
+}
+
+pub fn is_valid_name(name :&str)->bool{
+    let is_empty_or_whitespace = name.trim().is_empty();
+    let is_too_long = name.graphemes(true).count()> 256;
+    let forbidden_characters = ['/', '(',')', '"','<','>'];
+    let contains_forbidden_characters = name.contains(forbidden_characters);
+
+    !(is_empty_or_whitespace|| is_too_long || contains_forbidden_characters)
 }
