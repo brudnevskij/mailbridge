41bc0599343ac83327f0bd48afcbcb27051021d0
Segfaulter <brudnevskij@users.noreply.github.com>
2026-05-04 03:06:15 +0300

    feat: updated NewSubscriber with tryFrom trait

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -14,6 +14,18 @@ pub struct FormData {
     name: String,
 }
 
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(NewSubscriber{
+            name, email
+        })
+    }
+}
+
 #[tracing::instrument(
     name = "Adding new subscriber",
     skip(form, pool), 
@@ -22,15 +34,10 @@ pub struct FormData {
         subscriber_name = %form.name)
 )]
 pub async fn subscribe(form: Form<FormData>, pool: Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    let new_subscriber = match form.0.try_into() {
+        Ok(new_subscriber) => new_subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let email = match SubscriberEmail::parse(form.0.email){
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriber = NewSubscriber{ email, name  };
     match insert_subscriber(&pool, &new_subscriber).await
     {
         Ok(_) => HttpResponse::Ok().finish(),
