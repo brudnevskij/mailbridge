7d527415244483b9039511c28b527f700b0a6831
Segfaulter <brudnevskij@users.noreply.github.com>
2026-05-13 23:12:10 +0300

    feat: added email client mod

---
+++ src/email_client.rs
@@ -0,0 +1,29 @@
+use reqwest::Client;
+
+use crate::domain::SubscriberEmail;
+
+pub struct EmailClient {
+    sender: SubscriberEmail,
+    base_url: String,
+    http_client: Client,
+}
+
+impl EmailClient {
+    pub async fn send_email(
+        &self,
+        recipient: SubscriberEmail,
+        subject: &str,
+        html_content: &str,
+        text_content: &str,
+    ) -> Result<(), String> {
+        todo!()
+    }
+
+    pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
+        Self {
+            http_client: Client::new(),
+            base_url,
+            sender,
+        }
+    }
+}
